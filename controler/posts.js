113e99a5dd9bee6a524df23ba36025c5ddf218a1
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2024-05-22 18:16:31 +0530

    all controler move to controle folder

--- controler/posts.js
+++ controler/posts.js
@@ -13,6 +13,36 @@ function postInfo(req, res) {
         res.json(newpost);
     }
 
-    module.exports = {
+function specificPost (req, res){
+    const id = parseInt(req.params.id);
+    const vlog = posts.find(elm => elm.id === id);
+    if (vlog) {
+        res.json(vlog);
+    } else {
+        res.status(404).json({ error: 'Post not found' });
+    }
+}
+
+function editSpecificPost (req, res) {
+    const id = parseInt(req.params.id);
+    const postId = posts.findIndex(elem => elem.id === id);
+    posts[postId].title = req.body.title || posts[postId].title;
+    posts[postId].content = req.body.content || posts[postId].content;
+    posts[postId].author = req.body.author || posts[postId].author;
+    res.json(posts[postId]);
+}
+
+function deletePost (req, res) {
+    const id = parseInt(req.params.id);
+    const postId = posts.findIndex(elem => elem.id === id);
+    console.log(postId);
+    posts.splice(postId, 1);
+    res.json(posts);
+}
+
+module.exports = {
         postInfo,
+        specificPost,
+        editSpecificPost,
+        deletePost
     }
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { postInfo } = require("../controler/posts")
+const { postInfo,specificPost,editSpecificPost,deletePost } = require("../controler/posts")
 
 
 let lastId = 3;
@@ -17,29 +17,8 @@ router
     .post(postInfo);
 
 router.route("/:id")
-    .get((req, res) => {
-        const id = parseInt(req.params.id);
-        const vlog = posts.find(elm => elm.id === id);
-        if (vlog) {
-            res.json(vlog);
-        } else {
-            res.status(404).json({ error: 'Post not found' });
-        }
-    })
-    .patch((req, res) => {
-        const id = parseInt(req.params.id);
-        const postId = posts.findIndex(elem => elem.id === id);
-        posts[postId].title = req.body.title || posts[postId].title;
-        posts[postId].content = req.body.content || posts[postId].content;
-        posts[postId].author = req.body.author || posts[postId].author;
-        res.json(posts[postId]);
-    })
-    .delete((req, res) => {
-        const id = parseInt(req.params.id);
-        const postId = posts.findIndex(elem => elem.id === id);
-        console.log(postId);
-        posts.splice(postId, 1);
-        res.json(posts);
-    });
+    .get(specificPost)
+    .patch(editSpecificPost)
+    .delete(deletePost);
 
 module.exports = router;
